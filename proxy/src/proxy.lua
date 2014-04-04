63daddeda58f8bfcf9efb2ad4e5c507839c9d3b4
Wang Yi <mudone@gmail.com>
2014-04-04 21:44:27 +0800

    rename

--- proxy/src/proxy.lua
+++ proxy/src/proxy.lua
@@ -1,23 +1,78 @@
-local http = require "resty.http"
-local http_client = http:new()
-local url = "http://"
+local proxy = {}
+proxy.cjson = require "cjson"
+proxy.ssdb = require "resty.ssdb"
+proxy.db = proxy.ssdb:new()
 
-if ngx.var.http_host then
-	url = url .. ngx.var.http_host
+function proxy.db_connect()
+	local ok, err = proxy.db:connect("127.0.0.1", "8888")
+	if not ok then
+		ngx.say("500")
+		ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+		return
+	end
 end
 
-url = url .. ngx.var.request_uri
-
-if ngx.var.is_args then
-	url = url .. ngx.var.is_args .. ngx.var.args
+function proxy.fetch ()
+	local action = ngx.var.request_method
+	if action == "POST" then
+		method = ngx.HTTP_POST
+	elseif action == "GET" then 
+		method = ngx.HTTP_GET
+	elseif action == "HEAD" then
+		method = ngx.HTTP_HEAD
+	else
+		ngx.exit(ngx.HTTP_NOT_ALLOWED)
+	end
+	
+	res = ngx.location.capture (
+		'/proxy', {method = method, always_forward_body = true, copy_all_vars = true}
+	)
+	return res
 end
 
-local ok, code, headers, status, body = http_client:request {
-	url = "http://backend",
-	timeout = 30000,
-	headers = ngx.req.get_headers(),
-	method = ngx.var.request_method,
-}
+function proxy.response(res, x_cache)
+	for k,v in pairs(res.header) do
+		ngx.header[k] = v
+	end
+	ngx.header["X-Cache"] = x_cache
+	ngx.print(res.body)
+	ngx.exit(res.status)
+end
 
-ngx.print(headers)
-ngx.print(body)
+function proxy.get()
+	local args = ""
+	if ngx.var.args ~= nil then
+		args = ngx.var.args
+	end
+	
+	local cache_raw = ngx.var.host..ngx.var.request_uri..args
+	local cache_key = ngx.md5(cache_raw)
+	local cache_content, err = proxy.db:get(cache_key)
+	
+	if cache_content then
+		local res = ""
+		if type(cache_content) == "table" then
+			res = cache_content[1]
+		elseif type(cache_content) == "string" then
+			res = cache_content
+		end
+	
+		return proxy.cjson.decode(res), "HIT"
+	else
+		res = proxy.fetch()
+		if res.status == ngx.HTTP_OK then
+			db:set(cache_key, proxy.cjson.encode(res))
+			return res, "MISS"
+		end
+	end
+end
+
+function proxy.run()
+	proxy.db_connect()
+
+	local res, x_cache = proxy.get()
+	return proxy.response(res, x_cache)
+end
+
+proxy.run()
+
